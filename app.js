4d4accb3335017d01e004c80df1452c14a616269
Scott Reyell <smreyell@gmail.com>
2018-07-05 22:15:42 -0700

    blended youtube api with omdb api

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 
+$('.popup').hide()
+$('.overlayBg').hide()
+
 $('#submit').click(function(e){
 	e.preventDefault();
 	OMDBRequest();
@@ -9,6 +12,7 @@ function OMDBRequest(){
 		$.getJSON('https://www.omdbapi.com/?t='+$('#search').val()+'&y='+$('#year').val()+'&plot=full&apikey=eefeda9a',function(data){
 			console.log(data);
 			showResults(data);
+			searchYoutube();
 		})
 	})
 
@@ -19,22 +23,61 @@ function showResults(data){
 		<div class="col-4">
 			<h2>${data.Title}</h2>
 			<img src="${data.Poster}">
+			<p><strong>Plot</strong>: ${data.Plot}</p>
 		</div>
 		<div class="col-4">
-			<p>Plot: ${data.Plot}</p>
-			<p>Release Date: ${data.Released}</p>
-			<p>Runtime: ${data.Runtime}</p>
-			<p>Director: ${data.Director}</p>
-			<p>Writers: ${data.Writers}</p>
-			<p>Actors: ${data.Actors}</p>
-			<p>Awards: ${data.Awards}</p>
 			<p>${data.Ratings[0].Source}:<span>${data.Ratings[0].Value}</span></p>
 			<p>${data.Ratings[1].Source}<span>${data.Ratings[1].Value}</span></p>
 			<p>${data.Ratings[2].Source}<span>${data.Ratings[2].Value}</span></p>
+		</div>
+		<div class="col-4">
+			<p>Release Date: ${data.Released}</p>
+			<p>Runtime: ${data.Runtime}</p>
+			<p>Director: ${data.Director}</p>
+			<p>Actors: ${data.Actors}</p>
+			<p>Awards: ${data.Awards}</p>
 		</div>`)
 
 }
+
+var pageToken = {};
+
+function searchYoutube() {
+	let query = $('#search').val()+' '+'the movie';
+	$.ajax({
+	    url: 'https://www.googleapis.com/youtube/v3/search'
+	    , dataType: 'json'
+	    , type: 'GET'
+	    , data: {
+	        key: 'AIzaSyA9YIeJMUAUAO5QaCo0wzfbdGlLIbjo1D4'
+	        , q: query//$('#search').val()
+	        , part: 'snippet'
+	        , maxResults: 6
+	        , pageToken: pageToken.current
+	    }
+	}).done(function vidResults (data) {
+	    pageToken.nextPage = data.nextPageToken;
+	    pageToken.prevPage = data.prevPageToken;
+	    var html = "";
+	    $.each(data['items'], function (index, value) {
+	        html += '<div class="col-4"><p class="title">' + value.snippet.title + '</p>';
+	        html += '<p class="url"><a href="https://www.youtube.com/watch?v=' + value.id.videoId + '" target="_blank">' + value.id.videoId + '</a></p>';
+	        html += '<p><img  class="thumbnail" src="' + value.snippet.thumbnails.medium.url + '" videoID="' + value.id.videoId + '"></p>';
+	        html += '</div>';
+	    })
+	    $('#videoResults').html(html);
+	})
+}	
 	
-	
+$('.overlayBg').click(function () {
+    $('.popup').hide()
+    $('.overlayBg').hide()
+})
+$('#videoResults').on('click', '.thumbnail', function () {
+    $('.popup').show()
+    $('.overlayBg').show();
+    $(window).scrollTop(0)
+    $('.popup iframe').attr('src', 'https://www.youtube.com/embed/' + $(this).attr('videoID'));
+})
 
 
